1d84881162836a67a12ac43c63a7c00339ec464a
JM Dumlao <ansolo.dumlao@gmail.com>
2024-11-14 17:48:17 -0500

    initial comit
    initial

--- migrations/20241114175416_create_budget_table/migration.sql
+++ migrations/20241114175416_create_budget_table/migration.sql
@@ -1,8 +1,27 @@
--- CreateTable
-CREATE TABLE "Budget" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "amount" DOUBLE PRECISION NOT NULL,
+-- Create Budgets Table
+CREATE TABLE Budgets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    amount DECIMAL NOT NULL,
+    createdBy VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
 
-    CONSTRAINT "Budget_pkey" PRIMARY KEY ("id")
+-- Create Expenses Table
+CREATE TABLE Expenses (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    amount DECIMAL NOT NULL,
+    budgetId INT NOT NULL,
+    createdAt TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    CONSTRAINT fk_budget FOREIGN KEY (budgetId) REFERENCES Budgets(id) ON DELETE CASCADE
+);
+
+-- Create Incomes Table
+CREATE TABLE Incomes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    amount DECIMAL NOT NULL,
+    createdBy VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
