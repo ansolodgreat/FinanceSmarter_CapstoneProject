c3f0c09e3a0578f8826f925af9297d2a35b45899
JM Dumlao <ansolo.dumlao@gmail.com>
2024-12-16 16:02:11 -0500

    final
    final

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/jmstaceydumlao/Desktop/FinanceSmarter_CapstoneProject/app/layout\":[\"static/media/07a54048a9278940-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/jmstaceydumlao/Desktop/FinanceSmarter_CapstoneProject/app/layout":["static/media/07a54048a9278940-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
