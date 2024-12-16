3c98d17e41c1f64301a75fe6497d33b483ad575e
JM Dumlao <ansolo.dumlao@gmail.com>
2024-12-16 11:35:18 -0500

    final
    final

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner.jsx";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
